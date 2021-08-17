c7d96a5473da9ca7ad168951cbdb06a60e29fd72
Nishan <nishan@ekoapp.com>
2021-08-17 16:49:12 +0700

    update Package.swift for v5.5.0

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AmityVideoBroadcast",
+    products: [
+        .library(
+            name: "AmityVideoBroadcast",
+            targets: ["AmityLiveVideoBroadcastKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AmityLiveVideoBroadcastKit",
+            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-frameworks/5.5.0/AmityLiveVideoBroadcastKit.xcframework.zip",
+            checksum: "05099be5cf79c49c581176927744146d865ceba892a53293b6d7bceda9881159"
+        ),
+    ]
+)
