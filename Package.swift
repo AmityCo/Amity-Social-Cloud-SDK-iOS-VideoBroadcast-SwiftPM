3d583f1ca387b4d7024415d69b58acb9a1f55334
Zryte <>
2023-03-14 14:37:48 +0700

    update Package.swift for v6.2.1

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AmityVideoBroadcast",
+    products: [
+        .library(
+            name: "AmityVideoBroadcast",
+            targets: ["AmityLiveVideoBroadcastKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AmityLiveVideoBroadcastKit",
+            url: "https://sdk.amity.co/sdk-release/ios-frameworks/6.2.1/AmityLiveVideoBroadcastKit.xcframework.zip",
+            checksum: "45556d508bb473b12943404090ab870d816947a433f974e30e41432a42fff708"
+        ),
+    ]
+)
